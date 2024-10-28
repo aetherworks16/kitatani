ea906a931e158c7fdcca4b447a969daba8d0772e
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-10-28 18:31:46 +0700

    Refactor dashboard layout to include navbar and sidebar components

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Maps from "../components/maps";
+import Navbar from "../components/navbar";
+import Sidebar from "../components/sidebar";
 
 const Dashboard = () => {
   return (
-    <>
-      <Maps />
-    </>
+    <div className="flex h-screen overflow-hidden">
+      {/* Sidebar */}
+      <div className="w-64">
+        <Sidebar />
+      </div>
+
+      {/* Main Content */}
+      <div className="flex flex-col flex-grow">
+        <Navbar />
+        <div className="flex-grow w-full">
+          <Maps />
+        </div>
+      </div>
+    </div>
   );
 };
 
